4a943f5b31acf45eb050650785c5257984c8ec0a
Tim Lu <tim.lu@linecrop.com>
2024-12-27 17:45:11 +0800

    refined

--- test2.js
+++ test2.js
@@ -1,79 +1,145 @@
+// function isValid(s) {
+//   // N: length of s
+//   // filter out invalid characters, O(N)
+//   if (!/^([0-9()]*\s*(and|or)?\s*[0-9()]*)*$/i.test(s)) {
+//     return false
+//   }
+
+//   let idx = 0
+//   function parse(open) {
+//     let lastOp = null
+//     const stack = []
+//     while (idx < s.length) {
+//       switch (s[idx]) {
+//         case '(':
+//           idx += 1
+//           const item = parse(open + 1)
+//           if (typeof item !== 'number') {
+//             return null
+//           }
+//           stack.push(item)
+//           idx += 1
+//           break
+//         case 'o':
+//           idx += 2
+//           if (lastOp) {
+//             return null
+//           }
+//           lastOp = 'or'
+//           break
+//         case 'a':
+//           idx += 3
+//           if (lastOp) {
+//             return null
+//           }
+//           lastOp = 'and'
+//           break
+//         case ' ':
+//           idx += 1
+//           break
+//         case ')':
+//           if (open === 0 || lastOp) {
+//             return null
+//           }
+//           idx += 1
+//           return stack.reduce((acc, n) => acc + n, 0)
+//         default:
+//           let num = 0
+//           while (idx < s.length && /[0-9]/.test(s[idx])) {
+//             num = num * 10 + +s[idx++]
+//           }
+//           stack.push(num)
+//           if (lastOp) {
+//             if (stack.length < 2) {
+//               return null
+//             }
+//             stack.pop()
+//             stack.pop()
+//             stack.push(1)
+//             lastOp = null
+//           }
+//       }
+//     }
+//     if (open) {
+//       return null
+//     }
+//     if (lastOp && stack.length !== 2) {
+//       return null
+//     }
+//     return (lastOp && stack.length === 2) || stack.length <= 1
+//   }
+
+//   const r = parse(0)
+//   if (!r) {
+//     return false
+//   }
+//   return true
+// }
+
+// N: length of s
+// O(N)
 function isValid(s) {
-  // N: length of s
-  // filter out invalid characters, O(N)
   if (!/^([0-9()]*\s*(and|or)?\s*[0-9()]*)*$/i.test(s)) {
     return false
   }
 
   let idx = 0
   function parse(open) {
-    let lastOp = null
-    const stack = []
+    let lastOp = false
+    let deep = 0
     while (idx < s.length) {
       switch (s[idx]) {
         case '(':
           idx += 1
           const item = parse(open + 1)
           if (typeof item !== 'number') {
-            return null
+            return false
           }
-          stack.push(item)
+          deep += 1
           idx += 1
           break
         case 'o':
-          idx += 2
-          if (lastOp) {
-            return null
-          }
-          lastOp = 'or'
-          break
         case 'a':
-          idx += 3
-          if (lastOp) {
-            return null
+          if (s[idx] === 'o') {
+            idx += 2
+          } else {
+            idx += 3
           }
-          lastOp = 'and'
+          if (lastOp) {
+            return false
+          }
+          lastOp = true
           break
         case ' ':
           idx += 1
           break
         case ')':
           if (open === 0 || lastOp) {
-            return null
+            return false
           }
           idx += 1
-          return stack.reduce((acc, n) => acc + n, 0)
+          return Math.max(1, deep)
         default:
-          let num = 0
           while (idx < s.length && /[0-9]/.test(s[idx])) {
-            num = num * 10 + +s[idx++]
+            idx += 1
           }
-          stack.push(num)
+          deep += 1
           if (lastOp) {
-            if (stack.length < 2) {
-              return null
+            if (deep < 2) {
+              return false
             }
-            stack.pop()
-            stack.pop()
-            stack.push(1)
-            lastOp = null
+            deep -= 1
+            lastOp = false
           }
       }
     }
-    if (open) {
-      return null
+    if (open || (lastOp && deep !== 2)) {
+      return false
     }
-    if (lastOp && stack.length !== 2) {
-      return null
-    }
-    return (lastOp && stack.length === 2) || stack.length <= 1
+    return (lastOp && deep === 2) || deep <= 1
   }
 
-  const r = parse(0)
-  if (!r) {
-    return false
-  }
-  return true
+  return parse(0)
 }
 
 // true
